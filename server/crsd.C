24c4868bb01048322d6356d4e3cb4ad58185e568
Martin Fracker <martin.frackerjr@gmail.com>
2017-01-25 09:26:37 -0600

    Just hardcode crsd in the message

--- server/crsd.C
+++ server/crsd.C
@@ -9,8 +9,8 @@ int main(int argc, char* argv[]) {
   auto addr = LOCALHOST;
   auto port = bind_socket(master_socket, addr);
   printf("Chat server running at:\n\n\t %s:%d\n\n", addr, port);
-  printf("Run 'pkill %s' to kill the server.\n", argv[0]);
-  printf("Alternatively use 'ps' to get the PID\n\tand run 'pkill PID'.\n");
+  printf("Run 'pkill crsd' to kill the server.\n");
+  printf("Alternatively use 'ps' to get the PID,\n and run 'kill -KILL PID'.\n");
   fflush(stdout);
   if (fork() == 0) {
     process_commands(master_socket);
